055757e7a0b3a361fc5e99727b19863b37549251
Mostafa Esmail <100853809+MostafaEsma3il@users.noreply.github.com>
2023-10-09 19:35:29 +0300

    Update script.js

--- mini-projects/digital_clock/script.js
+++ mini-projects/digital_clock/script.js
@@ -1,7 +1,8 @@
 function updateTime () {
     let now = new Date();
     let timeString = now.toLocaleTimeString();
-
+    if (timeString.length == 10)
+        timeString = "0" + timeString;
     document.getElementById('time').innerText = timeString;
 }
 
